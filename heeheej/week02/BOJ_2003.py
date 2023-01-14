c803a4c5e30a105e37abcf2a6765d09417b665d4
heeheejj <kjyk0835@naver.com>
2023-01-15 01:28:26 +0900

    Week02 BOJ 2003 수들의 합 2

---
+++ heeheej/week02/BOJ_2003.py
@@ -0,0 +1,26 @@
+# 수들의 합 2
+# 240ms, 114488kb
+# 수열 A[]의 원소는 30000을 넘지 않는 자연수! => 투포인터
+
+import sys
+
+sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+inputs = list(map(int, input().split()))
+
+left, right = 0, 0
+result = 0
+for left in range(N):
+    sum = 0
+    right = left
+    while sum < M and right < N:
+        sum += inputs[right]
+
+        if sum < M:
+            right += 1
+        elif sum == M:
+            result += 1
+            break
+print(result)
