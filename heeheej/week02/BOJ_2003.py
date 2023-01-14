bdeb434367fdc4243bbd2f4fc550bbfeeb29fafc
heeheejj <kjyk0835@naver.com>
2023-01-15 01:40:36 +0900

    Week02 BOJ 2003 수들의 합 2 개선

--- heeheej/week02/BOJ_2003.py
+++ heeheej/week02/BOJ_2003.py
@@ -1,6 +1,7 @@
 # 수들의 합 2
 # 240ms, 114488kb
 # 수열 A[]의 원소는 30000을 넘지 않는 자연수! => 투포인터
+# sum은 매 반복문마다 0부터 더해 계산할 필요 없이, left위치의 원소만큼 빼고, 다시 다음 반복문의 right위치의 원소만큼 더하면 된다!
 
 import sys
 
@@ -12,15 +13,12 @@ inputs = list(map(int, input().split()))
 
 left, right = 0, 0
 result = 0
+sum = 0
 for left in range(N):
-    sum = 0
-    right = left
     while sum < M and right < N:
         sum += inputs[right]
-
-        if sum < M:
-            right += 1
-        elif sum == M:
-            result += 1
-            break
+        right += 1
+    if sum == M:
+        result += 1
+    sum -= inputs[left]
 print(result)
